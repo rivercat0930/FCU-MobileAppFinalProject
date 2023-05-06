6d033868c07258b3653e474ab825db0ad752f4a5
lauchloeyun <lauchloeyun@gmail.com>
2023-05-06 13:33:44 +0800

    UXUI更新

---
+++ App/FW_Courier/app/src/main/java/dev/rivercat/fw_courier/RestaurantSigninActivity.java
@@ -0,0 +1,14 @@
+package dev.rivercat.fw_courier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantSigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurantsignin);
+    }
+}
