bad3bc188eb789b58e38d594e9a6a7ec03d23203
rivercat0930 <h90093000054@gmail.com>
2023-05-06 16:59:08 +0800

    #12 完成登入and註冊部份

---
+++ Server/mobileAppServer/src/main/java/dev/rivercat/mobileAppServer/model/Account.java
@@ -0,0 +1,60 @@
+package dev.rivercat.mobileAppServer.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    public Account() {}
+
+    public Account(String account, String password, String email) {
+        this.username = account;
+        this.password = password;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
