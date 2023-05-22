3b884162ddc1818c2e7caacb651a26ba8d62f9f2
rivercat0930 <h90093000054@gmail.com>
2023-05-22 14:55:48 +0800

    #21 完成登入畫面功能
    已經完成登入功能，後端串接 Server 狀況穩定，也加入了一些提示錯誤訊息

--- App/FW_Courier/app/src/main/java/dev/rivercat/fw_courier/MainActivity.java
+++ App/FW_Courier/app/src/main/java/dev/rivercat/fw_courier/MainActivity.java
@@ -1,5 +1,6 @@
 package dev.rivercat.fw_courier;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,11 +9,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import dev.rivercat.fw_courier.connect.APIService;
+import dev.rivercat.fw_courier.connect.RetrofitManager;
+import dev.rivercat.fw_courier.module.LoginInformation;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private Button btnRegister;
     private Button btnRestaurant;
+    private APIService apiService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,16 +36,23 @@ public class MainActivity extends AppCompatActivity {
         btnRegister = findViewById(R.id.main_btn_register);
         btnRestaurant = findViewById(R.id.main_btn_restaurant);
 
+        apiService = RetrofitManager.getInstance().getAPI();
+
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(v.getId()==R.id.main_btn_login){
-                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                }else if(v.getId()==R.id.main_btn_restaurant){
+                // login button
+                if (v.getId() == R.id.main_btn_login) {
+                    if (etAccount.getText().toString().isEmpty() || etPassword.getText().toString().isEmpty())
+                        Toast.makeText(MainActivity.this, "請輸入帳號密碼", Toast.LENGTH_SHORT).show();
+                    else
+                        dealWithLoginEvent(etAccount.getText().toString(), etPassword.getText().toString());
+                }
+                else if (v.getId() == R.id.main_btn_restaurant) {
                     Intent intent = new Intent(MainActivity.this, RestaurantSigninActivity.class);
                     startActivity(intent);
-                }else if(v.getId()==R.id.main_btn_register){
+                }
+                else if (v.getId() == R.id.main_btn_register) {
                     Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                     startActivity(intent);
                 }
@@ -52,7 +62,33 @@ public class MainActivity extends AppCompatActivity {
         btnRegister.setOnClickListener(onClickListener);
         btnRestaurant.setOnClickListener(onClickListener);
         btnLogin.setOnClickListener(onClickListener);
+    }
 
+    private void dealWithLoginEvent(String username, String password) {
+        LoginInformation loginInformation = new LoginInformation(username, password);
+        Call<Void> call = apiService.login(loginInformation);
 
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                System.out.println(response.code());
+                if (response.code() == 200) {
+                    Toast.makeText(MainActivity.this, "登入成功", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+
+                if (response.code() == 404)
+                    Toast.makeText(MainActivity.this, "使用者名稱或密碼錯誤，請確認是否註冊", Toast.LENGTH_SHORT).show();
+
+                if (response.code() == 403)
+                    Toast.makeText(MainActivity.this, "使用者名稱或密碼錯誤，請確認是否註冊", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                System.out.println("ERROR: " + t);
+            }
+        });
     }
 }
