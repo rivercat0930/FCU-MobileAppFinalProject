d79a087701102e75c8ece9be7733825ca440df13
BearWithSheep <joyce910418@gmail.com>
2023-05-24 21:29:13 +0800

    RegisterInformation使用者資訊登入
    導入使用者資訊
    能順利運作

--- App/FW_Courier/app/src/main/java/dev/rivercat/fw_courier/module/RegisterInformation.java
+++ App/FW_Courier/app/src/main/java/dev/rivercat/fw_courier/module/RegisterInformation.java
@@ -3,23 +3,17 @@ package dev.rivercat.fw_courier.module;
 public class RegisterInformation {
     String username;
     String password;
-    String email;
 
-    public RegisterInformation(String username, String password, String email) {
+    public RegisterInformation(String username, String password) {
         this.username = username;
         this.password = password;
-        this.email = email;
     }
 
     public String getUsername() {
         return username;
     }
-
     public String getPassword() {
         return password;
     }
 
-    public String getEmail() {
-        return email;
-    }
 }
