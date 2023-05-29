45a1466b9d4aa5ed1a6d014330a95a424c56a4de
RW017 <lauchloeyun@gmail.com>
2023-05-29 14:06:45 +0800

    #19 連線傳送

---
+++ App/FW_Courier/app/src/main/java/dev/rivercat/fw_courier/module/Restaurantformation.java
@@ -0,0 +1,54 @@
+package dev.rivercat.fw_courier.module;
+
+import androidx.annotation.NonNull;
+
+  public class RegisterInformation {
+
+    private String restaurantName;
+    private Double rate;
+    private String description;
+
+
+
+    public RegisterInformation(String restaurantName, Double rate, String description) {
+      this.restaurantName = restaurantName;
+      this.rate = rate;
+      this.description = description;
+
+    }
+
+    public String getRestaurantName() {
+      return restaurantName;
+    }
+
+    public Double getRate() {
+      return rate;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+      this.restaurantName = restaurantName;
+    }
+
+    public void setRate(Double rate) {
+      this.rate = rate;
+    }
+
+    public void setDescription(String description) {
+      this.description = description;
+    }
+
+    public String getDescription()
+        return description;
+
+    @NonNull
+    @Override
+    public String toString() {
+      return "RegisterInformation{" +
+        "restaurantName='"+restaurantName+'\''+
+        ",ratr="+rate+
+        ",description='"+description+'\''+
+        '}';
+    }
+
+  }
+  
---
+++ App/FW_Courier/app/src/main/java/dev/rivercat/fw_courier/view/RestaurantView.java
@@ -0,0 +1,14 @@
+package dev.rivercat.fw_courier.view;
+
+import android.content.Context;
+import android.widget.BaseAdapter;
+
+import dev.rivercat.fw_courier.module.Restaurantformation;
+
+public class RestaurantView {
+
+  public class RestaurantView extends BaseAdapter{
+    private Context context;
+    private ArrayList<Restaurantformation>
+  }
+}
