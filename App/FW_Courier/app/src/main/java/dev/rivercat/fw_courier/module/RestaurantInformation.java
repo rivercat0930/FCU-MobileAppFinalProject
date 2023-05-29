1d69eda3159737e9fd9efd1ae0046e015fd21700
RW017 <lauchloeyun@gmail.com>
2023-05-29 14:18:43 +0800

    #19 好耶 基本寫完 但現在一堆紅紅的要debug 救命

--- App/FW_Courier/app/src/main/java/dev/rivercat/fw_courier/module/RestaurantInformation.java
+++ App/FW_Courier/app/src/main/java/dev/rivercat/fw_courier/module/RestaurantInformation.java
@@ -11,7 +11,7 @@ public class RestaurantInformation {
 
 
 
-    public RegisterInformation(String restaurantName, Double rate, String description) {
+    public Restaurantformation(String restaurantName, Double rate, String description) {
       this.restaurantName = restaurantName;
       this.rate = rate;
       this.description = description;
@@ -22,24 +22,28 @@ public class RestaurantInformation {
       return restaurantName;
     }
 
-    public Double getRate() {
-      return rate;
-    }
-
     public void setRestaurantName(String restaurantName) {
       this.restaurantName = restaurantName;
+    }
+
+
+    public Double getRate() {
+      return rate;
     }
 
     public void setRate(Double rate) {
       this.rate = rate;
     }
 
+
+    public String getDescription() {
+      return description;
+    }
+
     public void setDescription(String description) {
       this.description = description;
     }
 
-    public String getDescription()
-        return description;
 
     @NonNull
     @Override
